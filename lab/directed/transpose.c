6d53d2a661c3b9fc7ae5ebf6eeb36d389b40ff5f
Brian Park <briancpark@berkeley.edu>
2022-02-27 16:59:41 -0800

    push progress

--- lab/directed/transpose.c
+++ lab/directed/transpose.c
@@ -6,12 +6,25 @@
    verification for shorter simulations */
 #define VERIFY
 
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
 void __attribute__ ((noinline)) transpose(mat_nxm_t dst, const mat_mxn_t src)
 {
-    size_t i, j;
-    for (i = 0; i < MAT_M; i++) {
-        for (j = 0; j < MAT_N; j++) {
-            dst[j][i] = src[i][j];
+    size_t i, j, l, k;
+    // for (i = 0; i < MAT_M; i++) {
+    //     for (j = 0; j < MAT_N; j++) {
+    //         dst[j][i] = src[i][j];
+    //     }
+    // }
+
+    size_t blocksize = 8;
+    for (i = 0; i < MAT_M; i += blocksize) {
+        for (j = 0; j < MAT_N; j += blocksize) {
+            for (k = i; k < min(i + blocksize, MAT_M); ++k) {
+                for (l = j; l < min(j + blocksize, MAT_N); ++l) {
+                    dst[l][k] = src[k][l];
+                }
+            }
         }
     }
 }
